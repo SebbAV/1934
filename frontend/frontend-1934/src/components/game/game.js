9dc343a20da2d40fcb57a9bc8488493c9847bf67
Sebastian Arredondo <jsebastianav8@gmail.com>
2018-11-12 17:33:21 -0600

    Added pause menu and fixed issues with bundle js

--- frontend/frontend-1934/src/components/game/game.js
+++ frontend/frontend-1934/src/components/game/game.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-import {postScores} from '../../actions/index'
+import { postScores } from '../../actions/index'
+import Modal from 'react-modal';
+import '../../css/logo.css'
 
 const color = ["red", "green", "yellow", "blue"];
 const number = ["1", "2", "3", "4"];
 const figure = ["circle", "triangle", "square", "poli"];
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    },
+    overlay: { zIndex: 1000 }
+};
+Modal.setAppElement('.container')
 class Game extends Component {
     constructor(props) {
         super(props)
@@ -16,11 +30,21 @@ class Game extends Component {
                 down: "",
                 finished: false,
                 doors: [false, false, false, false],
-                movements: []
+                movements: [],
+                modalIsOpen: false
             }
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
         this.selectDoor = this.selectDoor.bind(this);
         this.sortDoors = this.sortDoors.bind(this);
     }
+    openModal() {
+        this.setState({ modalIsOpen: true });
+    }
+    closeModal() {
+        this.setState({ modalIsOpen: false });
+    }
+
     selectDoor(param) {
         switch (param.target.id) {
             case "door_up":
@@ -68,12 +92,12 @@ class Game extends Component {
                 right: number[Math.floor((Math.random() * 4) + 0)] + "_" + color[Math.floor((Math.random() * 4) + 0)] + "_" + figure[Math.floor((Math.random() * 4) + 0)] + ".png",
                 down: number[Math.floor((Math.random() * 4) + 0)] + "_" + color[Math.floor((Math.random() * 4) + 0)] + "_" + figure[Math.floor((Math.random() * 4) + 0)] + ".png"
             });
-        }else{
-            let {movements} = this.state
+        } else {
+            let { movements } = this.state
             let completed = true
             let user_object_uid = 1
             alert("game over");
-            this.props.postScores(({movements,user_object_uid,completed}), ()=> {
+            this.props.postScores(({ movements, user_object_uid, completed }), () => {
 
             })
         }
@@ -83,25 +107,42 @@ class Game extends Component {
     }
     render() {
         console.log(this.state.up)
-        if(this.state.up){
-        return (
-            <div>
-            <div className="col-md-12">
-                <button className="float-right" >Log out</button>
-            </div>
-                <div className="col-md-12">
-                    <img src={require(`../../img/doors/${this.state.up}`)} id="door_up" onClick={this.selectDoor} height="450"/>
-                    <img src={require(`../../img/doors/${this.state.left}`)} id="door_left" onClick={this.selectDoor} height="450"/>
-                    <img src={require(`../../img/doors/${this.state.right}`)} id="door_right" onClick={this.selectDoor} height="450"/>
-                    <img src={require(`../../img/doors/${this.state.down}`)} id="door_down" onClick={this.selectDoor} height="450"/>
+        if (this.state.up) {
+            return (
+                <div>
+                    <Modal
+                        isOpen={this.state.modalIsOpen}
+                        onAfterOpen={this.afterOpenModal}
+                        onRequestClose={this.closeModal}
+                        style={customStyles}
+                        contentLabel="Example Modal"
+                    >
+                        <div className="col-md-12 text-center">
+                            <button onClick={this.closeModal}>Continue</button>
+                        </div>
+                        <div className="col-md-12">
+                            <button >Scores</button>
+                        </div>
+                        <div className="col-md-12">
+                            <button >Exit</button>
+                        </div>
+                    </Modal>
+                    <div className="col-md-12">
+                        <button className="float-right" onClick={this.openModal} >Log out</button>
+                    </div>
+                    <div className="col-md-12">
+                        <img src={require(`../../img/doors/${this.state.up}`)} id="door_up" onClick={this.selectDoor} height="450" />
+                        <img src={require(`../../img/doors/${this.state.left}`)} id="door_left" onClick={this.selectDoor} height="450" />
+                        <img src={require(`../../img/doors/${this.state.right}`)} id="door_right" onClick={this.selectDoor} height="450" />
+                        <img src={require(`../../img/doors/${this.state.down}`)} id="door_down" onClick={this.selectDoor} height="450" />
+                    </div>
                 </div>
-            </div>
-        )
+            )
         }
         else {
-            return('Loading...')
+            return ('Loading...')
         }
     }
 }
 
-export default connect(null, {postScores})(Game)
+export default connect(null, { postScores })(Game)
